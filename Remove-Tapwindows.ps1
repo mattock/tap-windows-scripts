e99d7d0d55ec689ff76db907ce26fdca65a62e40
Samuli Seppänen <samuli@openvpn.net>
2020-08-28 15:39:01 +0300

    Support removing Wintun adapters as well
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- Remove-Tapwindows.ps1
+++ Remove-Tapwindows.ps1
@@ -4,19 +4,27 @@
 
 Param(
     [switch]$Yes,
+    [switch]$Wintun,
     [switch]$Help
 )
 
 Function Show-Usage {
     Write-Host
-    Write-Host "Usage: Remove-Tapwindows.ps1 [-Yes] [-Help]"
+    Write-Host "Usage: Remove-Tapwindows.ps1 [-Wintun] [-Yes] [-Help]"
     Write-Host
     Write-Host "Parameters:"
     Write-Host "    -Yes    Remove drivers instead of just showing what would get removed"
+    Write-Host "    -Wintun Remove Wintun drivers instead of tap-windows6 drivers"
     Write-Host "    -Help   Display this help message"
     Write-Host
     Write-host "Disclaimer: USE AT YOUR OWN RISK."
     exit 1
+}
+
+if ($Wintun) {
+    $provider = "WireGuard LLC"
+} else {
+    $provider = "TAP-Windows Provider V9"
 }
 
 $driverlist = Invoke-Command -ScriptBlock { & C:\Windows\System32\PnPutil.exe -e }
@@ -31,7 +39,7 @@ foreach ($line in $driverlist) {
     if ($line.StartsWith("Published name")) {
         $published_name = $line.Replace(" ","").Split(":")[1]
     } elseif ($line.StartsWith("Driver package provider")) {
-        if ($line.EndsWith("TAP-Windows Provider V9")) {
+        if ($line.EndsWith($provider)) {
             $tap_found = $true
             if ($Yes) { & PnPutil.exe -d $published_name }
             else      { Write-Host "Would remove ${published_name}" }
